7c01475accca77320e1e79eb20d5f9c496ba427e
karenvaldez <karen.valdez@galileo.edu>
2020-09-13 00:16:15 -0600

    Update TCP_Serve.js

--- TCP_Serve.js
+++ TCP_Serve.js
@@ -22,5 +22,61 @@ server.on('connection', function(server) {
     });
 });
 
+function header(sourcePort, destinationPort, sequenceNumber, acknowNumber) {
+    var headerFormat = '';
+
+
+    function ToHex(str) {
+        var arr = [];
+        for (var i = 0, l = str.length; i < l; i++) {
+            var hex = Number(str.charCodeAt(i)).toString(16);
+            arr.push(hex);
+        }
+        return arr.join('');
+    }
+
+    function complement(number, length) {
+
+        var my_string = '' + number;
+        while (my_string.length < length) {
+            my_string = '0' + my_string;
+        }
+
+        return my_string;
+
+    }
+
+    headerFormat = ToHex(sourcePort);
+    headerFormat += ToHex(destinationPort);
+   
+
+    if(typeof(sequenceNumber) == 'number'){
+        sequenceNumber = complement(sequenceNumber, 8)
+    } else {
+        sequenceNumber = complement(ToHex(sequenceNumber, 8))
+    }
+
+    headerFormat += sequenceNumber;
+
+    if(typeof(acknowNumber)== 'number'){
+        acknowNumber = complement(acknowNumber,8)
+    }else{
+        acknowNumber = complement(ToHex(acknowNumber, 8))
+    }
+
+    headerFormat += acknowNumber;
+
+    headerFormat +='5';
+    headerFormat += '000';
+    headerFormat += '0001';
+    headerFormat += '0000';
+    headerFormat += '0000';
+
+    return headerFormat;
+
+}
+
+var tcpheaderFormat = createHeader( sequenceNumber, acknowNumber );
+
 server.listen(8080, '192.168.1.56');
 console.log('Server Listening on 127.0.0.1:8080');
